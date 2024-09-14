205bfa51db90182121e43ad507097e1fb357aff8
Hadia Inam <hadia.inam@outlook.com>
2024-09-14 22:38:53 +0500

    Create middleware and a function to handle errors

--- API/index.js
+++ API/index.js
@@ -22,3 +22,14 @@ app.listen(3000, () => {
 
 app.use('/API/user', userRoutes);
 app.use('/API/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        sucess: false,
+        statusCode,
+        message,
+    });
+
+});
