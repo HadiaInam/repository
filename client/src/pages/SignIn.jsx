c81cd1eb81b4e911fadf8e231e65efa1b179b46f
Hadia Inam <hadia.inam@outlook.com>
2024-09-18 22:15:36 +0500

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,16 @@
 import { Link, useNavigate} from 'react-router-dom'
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const[errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading, error: errorMessage} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
+  
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value.trim() })
@@ -15,11 +18,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(!formData.password || !formData.email){
-      return setErrorMessage('Please fill out all fields. ')
+      return dispatch(signInFailure('Please fill out all fields. '));
     }
     try{
-      setLoading(true)
-      setErrorMessage(null)
+      dispatch(signInStart())
       const res = await fetch('/API/auth/signin', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -28,17 +30,16 @@ export default function SignIn() {
       );
       const data = await res.json();
       if(data.success === false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok){
+      
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
 
-    } catch(error){
-      setErrorMessage(error.message);
-      setLoading(false);
-
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
 
   }
