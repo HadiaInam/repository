338b10c97ddb5a4fc8440c73416ec949de2e7ed1
Hadia Inam <hadia.inam@outlook.com>
2024-09-28 16:53:58 -0500

    Complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,22 @@
 import { Navbar, TextInput, Button, NavbarLink, Dropdown, Avatar} from "flowbite-react";
 import { Link, useLocation } from "react-router-dom"; // Go to the page without refreshing it
 import {AiOutlineSearch} from "react-icons/ai";
-import { FaMoon } from "react-icons/fa"
-import {useSelector} from 'react-redux'
+import { FaMoon, FaSun } from "react-icons/fa"
+import {useSelector, useDispatch} from 'react-redux'
+import { toggleTheme } from "../redux/theme/themeSlice";
+
 
 //self-center whitespace-nowrap text-sm sm:text-xl font=semi-bold dark:text-white
 // border-b-2 is to set the bottom border
 //px-2 py-1 bg-gradient-to-r from from-indigo-500 via-purple-500 to-pink-500 rounded-lg
 export default function Header() {
    const path = useLocation().pathname;
+   const dispatch = useDispatch()
    const {currentUser} = useSelector(state => state.user)
-  return (
+   const {theme} = useSelector((state) => state.theme);
+
+
+   return (
     <Navbar className="border-b-2"> 
          <Link to="/" className='self-center text-sm sm:text-xl font=semi-bold whitespace-nowrap' >
             <span className="px-2 py-1 bg-gradient-to-r from from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Hadia's</span>
@@ -21,8 +27,8 @@ export default function Header() {
         </form>
         <Button pill className="w-12 h-10 lg:hidden" color='gray'><AiOutlineSearch /></Button>
         <div className="flex gap-2 md:order-2">
-          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-            <FaMoon />
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
+            {theme === 'light' ? <FaSun /> : <FaMoon/>}
           </Button>
           {currentUser ? (
             <Dropdown arrowIcon={false} inline label={
