15203cc9a12bebeb116c71f6d9d8a3e5c3667bf8
Hadia Inam <hadia.inam@outlook.com>
2024-10-07 12:47:48 -0500

    Add delete functionality to comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import {FaThumbsUp} from 'react-icons/fa'
 import { useSelector } from "react-redux"
 import { Button, Textarea } from "flowbite-react"
 
-export default function Comment({comment, onLike, onEdit}) {
+export default function Comment({comment, onLike, onEdit, onDelete}) {
     const [user, setUser] = useState({})
     const [editedContent, setEditedContent] = useState(comment.content)
     const {currentUser} = useSelector((state) => state.user)
@@ -52,7 +52,10 @@ export default function Comment({comment, onLike, onEdit}) {
          console.log(error.message)
         } 
     }
+    
+    const handleDelete = () => {
 
+    }
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
         <div className="flex-shrink-0 mr-3">
@@ -81,6 +84,9 @@ export default function Comment({comment, onLike, onEdit}) {
                     <p>{comment.numberOfLikes > 0 && comment.numberOfLikes + (comment.numberOfLikes === 1 ? " like" : " likes")}</p>
                     {currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
                         <button type='button' onClick={handleEdit} className='text-gray-400 hover:text-blue-500'>Edit</button>
+                    )}
+                     {currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                        <button type='button' onClick={() => onDelete(comment._id)} className='text-gray-400 hover:text-red-500'>Delete</button>
                     )}
                  </div>
                  </>
