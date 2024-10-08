1740642ec8ad56fdb3f63b2d9ed093ff657f64ca
Hadia Inam <hadia.inam@outlook.com>
2024-10-07 21:00:05 -0500

    Add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import {Sidebar} from 'flowbite-react'
 import { useEffect, useState } from 'react'
-import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
 import {Link, useLocation} from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
@@ -37,6 +37,15 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+
+                {
+                  currentUser.isAdmin && 
+                  <Link to='/dashboard?tab=dash'>
+                    <Sidebar.Item active={tab === 'dash'} icon={ HiChartPie } labelColor='dark' as='div'>
+                        Dashboard
+                    </Sidebar.Item>
+                  </Link>
+                }
                 <Link to='/dashboard?tab=profile'>
                     <Sidebar.Item active={tab === 'profile'} icon={ HiUser } label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
                         Profile
@@ -66,6 +75,7 @@ export default function DashSidebar() {
                     </Sidebar.Item>
                   </Link>
                 }
+                
                 
                 
                 <Sidebar.Item icon={ HiArrowSmRight } className= 'cursor-pointer' onClick={handleSignout}>
