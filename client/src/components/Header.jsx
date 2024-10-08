483e556ff71fc1e659c6ea51bcbd114355103bde
Hadia Inam <hadia.inam@outlook.com>
2024-10-08 13:53:58 -0500

    Complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Navbar, TextInput, Button, NavbarLink, Dropdown, Avatar} from "flowbite-react";
-import { Link, useLocation } from "react-router-dom"; // Go to the page without refreshing it
+import { Link, useLocation, useNavigate } from "react-router-dom"; // Go to the page without refreshing it
 import {AiOutlineSearch} from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa"
 import {useSelector, useDispatch} from 'react-redux'
 import { toggleTheme } from "../redux/theme/themeSlice";
 import { signoutSuccess } from "../redux/user/userSlice";
+import { useEffect, useState } from "react";
 
 
 //self-center whitespace-nowrap text-sm sm:text-xl font=semi-bold dark:text-white
@@ -12,9 +13,29 @@ import { signoutSuccess } from "../redux/user/userSlice";
 //px-2 py-1 bg-gradient-to-r from from-indigo-500 via-purple-500 to-pink-500 rounded-lg
 export default function Header() {
    const path = useLocation().pathname;
+   const location = useLocation()
    const dispatch = useDispatch()
+   const navigate = useNavigate()
    const {currentUser} = useSelector(state => state.user)
    const {theme} = useSelector((state) => state.theme);
+   const [searchTerm, setSearchTerm] = useState('')
+
+   useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if (searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+   }, [location.search])
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
    const handleSignout = async () => {
     try{
       const res = await fetch('/API/user/signout', {
@@ -38,8 +59,8 @@ export default function Header() {
             <span className="px-2 py-1 bg-gradient-to-r from from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Hadia's</span>
             Blog
          </Link>
-        <form>
-        <TextInput type='text' placeholder='Search' rightIcon={AiOutlineSearch} className="hidden lg:inline" />
+        <form onSubmit={handleSubmit}>
+        <TextInput type='text' placeholder='Search' rightIcon={AiOutlineSearch} value={searchTerm} className="hidden lg:inline" onChange={(e) => setSearchTerm(e.target.value)}/>
         </form>
         <Button pill className="w-12 h-10 lg:hidden" color='gray'><AiOutlineSearch /></Button>
         <div className="flex gap-2 md:order-2">
