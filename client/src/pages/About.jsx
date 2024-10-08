f43bcaf2581e512843679915d44cb9d8c85db831
Hadia Inam <hadia.inam@outlook.com>
2024-10-08 14:28:14 -0500

    Complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">About Hadia's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Hadia's Blog is a blog that i created to share my thoughts and ideas to the world. I am learning MERN stack and love to write about my experiences and things I learn throughout my journey.
+              I hope you enjoy reading my blog!
+            </p>
+            <p>On this blog, you will find weekly articles and tutorials on topics such as web development, software engineering, and programming languages. Hadia is always learninga nd exploring new technologies, so be sure to check back for new content!</p>
+            <p>We encourage you to leave comments on our posts and engage with other readers. You can like other people's comment and reply to them as well. We believe that a community of learners can help each other grow and improve.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
